de27c3f1dccfb735d2de68be441ad490053fe849
Noor Madkour <noormadkour@gmail.com>
2023-10-30 11:44:21 -0500

    boilerplate code added from previous modules, retrofit to work here

---
+++ views/__init__.py
@@ -0,0 +1,5 @@
+"""VIEWS IMPORT"""
+from .metals import MetalsView
+from .sizes import SizesView
+from .styles import StylesView
+from .orders import OrdersView
---
+++ views/metals.py
@@ -0,0 +1,27 @@
+"""Metal options module"""
+import json
+from nss_handler import status
+from repository import db_get_all, db_get_single
+
+
+class MetalsView:
+    def get(self, handler, url):
+        """ Method for handling HTTP request to GET all metals @ /metals
+        Args:
+            handler: http handler to handle the request and return a response
+            url: will be indexed into for query_params, and pk
+        Returns:
+            response from handler        
+        """
+        sql = "SELECT m.id, m.metal, m.price FROM Metals m"
+
+        if url["pk"] != 0:
+            sql += " WHERE m.id = ?"
+            query_results = db_get_single(sql, url["pk"])
+            serialized_metals = json.dumps(dict(query_results))
+        else:
+            query_results = db_get_all(sql, url["pk"])
+            metals = [dict(row) for row in query_results]
+            serialized_metals = json.dumps(dict(metals))
+
+        return handler.response(serialized_metals, status.HTTP_200_SUCCESS.value)
